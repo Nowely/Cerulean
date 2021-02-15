66d95a727b09a023cdbf54a896b05246fed43ebd
Ruslan <ilyin-r@mail.ru>
2021-02-16 01:47:44 +0300

    Add users button

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import {AppBar, Tabs, Tab, Toolbar, Container, IconButton, Typography} from '@material-ui/core';
+import {AppBar, Tabs, Tab, Toolbar, Container, IconButton, Typography, Button} from '@material-ui/core';
 //import {WhatshotRoundedIcon} from '@material-ui/icons';
 import WhatshotRoundedIcon from '@material-ui/icons/WhatshotRounded';
 import {useStyles} from '../styles'
-import {Switch, Route, Link, BrowserRouter} from "react-router-dom";
+import {Route, Link} from "react-router-dom";
+
+import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
 export const Layout = props => {
 	const classes = useStyles();
 
 	const [value, setValue] = React.useState(0);
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+
 
 	const handleChange = (event, newValue) => {
 		console.log(newValue);
 		setValue(newValue);
+	};
+
+	const handleMenu = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
 	};
 
 	return (
@@ -25,11 +40,6 @@ export const Layout = props => {
 					<Typography className={classes.title} variant="h6" noWrap>
 						Cerulean
 					</Typography>
-					{/*<Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-					<LinkTab label="Задачи" href="/" {...a11yProps(0)} />
-					<LinkTab label="Counter" href="/counter" {...a11yProps(1)} />
-					<LinkTab label="Fetch data" href="/fetch-data" {...a11yProps(2)} />
-					</Tabs>*/}
 					<Route
 						path="/"
 						render={({location}) => (
@@ -37,16 +47,38 @@ export const Layout = props => {
 								<Tabs value={value} onChange={handleChange}>
 									<Tab label="Задачи" component={Link} to="/" {...a11yProps(0)}/>
 									<Tab label="Счетчик" component={Link} to="/counter" {...a11yProps(1)}/>
-									{/*<Tab label="Item Three" href="#basic-tabs" component={Link} to="/tab3"/>*/}
 								</Tabs>
-								{/*<Switch>
-									<Route path="/tab2" render={() => <div>Tab 2</div>} />
-									<Route path="/tab3" render={() => <div>Tab 3</div>} />
-									<Route path="/" render={() => <div>Tab 1</div>} />
-								</Switch>*/}
 							</>
-						)}
-					/>
+						)}/>
+
+					<IconButton
+						style={{right: 0, position: 'absolute'}}
+						aria-label="account of current user"
+						aria-controls="menu-appbar"
+						aria-haspopup="true"
+						onClick={handleMenu}
+						color="inherit"
+					>
+						<PersonRoundedIcon />
+					</IconButton>
+					<Menu
+						id="menu-appbar"
+						anchorEl={anchorEl}
+						anchorOrigin={{
+							vertical: 'top',
+							horizontal: 'right',
+						}}
+						keepMounted
+						transformOrigin={{
+							vertical: 'top',
+							horizontal: 'right',
+						}}
+						open={open}
+						onClose={handleClose}
+					>
+						<MenuItem onClick={handleClose}>Profile</MenuItem>
+						<MenuItem onClick={handleClose}>My account</MenuItem>
+					</Menu>
 				</Toolbar>
 			</AppBar>
 			<Container>
