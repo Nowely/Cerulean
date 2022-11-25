ce599b78c4dfc9dcba9599b3056b527e2ca2c18b
Ruslan <ilyin-r@mail.ru>
2022-11-25 07:13:18 +0300

    Add comment

--- Data/Context.cs
+++ Data/Context.cs
@@ -7,4 +7,14 @@ public class Context : DbContext {
 	public Context(DbContextOptions<Context> options) : base(options) { }
 
 	public DbSet<Affair> Affair { get; set; }
+
+	/*protected override void OnModelCreating(ModelBuilder modelBuilder) {
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.Entity<Affair>()
+			.Property(x => x.Tags)
+			.HasConversion(new ValueConverter<List<string>, string>(
+				v => JsonSerializer.Serialize(v, JsonSerializerOptions.Default),
+				v => JsonSerializer.Deserialize<List<string>>(v, JsonSerializerOptions.Default)
+				)); 
+	}*/
 }
