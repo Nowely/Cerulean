2933127e35389bdb3317aae4857be9e46f6803b1
Ruslan <ilyin-r@mail.ru>
2024-01-11 00:49:15 +0300

    Clear

--- Cerulean.Aspire.AppHost/Program.cs
+++ Cerulean.Aspire.AppHost/Program.cs
@@ -5,7 +5,7 @@ var postgres = builder.AddPostgres("CeruleanDB").AddDatabase("Postgres");
 var gateway =  builder.AddProject<Projects.Cerulean_Gateway>("Gateway")
 					  .WithReference(postgres);
 
-builder.AddNpmApp("Client", "../reactapp1.client", "dev")
+builder.AddNpmApp("Client", "../Client", "dev")
 	   .WithReference(gateway)
 	   //.WithReference(cache)
 	   .WithServiceBinding(containerPort: 3000, scheme: "https", env: "PORT")
