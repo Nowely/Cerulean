08cb4b56eb5b08f4a3dda0a9b184246682856eea
Ruslan <ilyin-r@mail.ru>
2024-01-14 23:32:45 +0300

    Add web app service

--- Server/Cerulean.Service.WebApp/Program.cs
+++ Server/Cerulean.Service.WebApp/Program.cs
@@ -1,19 +1,19 @@
+using Cerulean.Aspire.ServiceDefaults;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 builder.AddDefaultOpenApi();
-builder.AddApplicationServices();
-
-builder.Services.AddControllers();
 
 var app = builder.Build();
 
-app.UseDefaultOpenApi();
-
 app.MapDefaultEndpoints();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
-app.MapControllers();
+app.MapFallbackToFile("/index.html");
 
 app.Run();
