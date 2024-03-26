363c168628be35a138213c3dec447e4fe8dd72d7
Ruslan <ilyin-r@mail.ru>
2024-03-27 01:30:04 +0300

    Clear

--- Server/Tools.Filtration/Models/Operators/StringOperator.cs
+++ Server/Tools.Filtration/Models/Operators/StringOperator.cs
@@ -29,27 +29,4 @@ public record StringOperator(StringOperatorType Type, string? Value = null) : IF
 			_ => null
 		};
 	}
-
-	/*if (Type == StringOperatorType.Is)
-			expression =
-				new StringFilterOptionsAttribute(StringFilterOption.Equals) { Comparison = Compare }.BuildExpression(
-					ContextFor(context, nameof(Equals), Equals));
-
-		if (Type == StringOperatorType.Contains)
-			expression =
-				new StringFilterOptionsAttribute(StringFilterOption.Contains) { Comparison = Compare }.BuildExpression(
-					ContextFor(context, nameof(Contains), Contains));
-
-		if (Type == StringOperatorType.NotContains)
-			expression = Expression.Not(new StringFilterOptionsAttribute(StringFilterOption.Contains) { Comparison = Compare }.BuildExpression(ContextFor(context, nameof(NotContains), NotContains)));
-
-		if (Type == StringOperatorType.StartsWith)
-			expression = new StringFilterOptionsAttribute(StringFilterOption.StartsWith) { Comparison = Compare }.BuildExpression(ContextFor(context, nameof(StartsWith), StartsWith));
-
-		if (Type == StringOperatorType.EndsWith)
-			expression = new StringFilterOptionsAttribute(StringFilterOption.EndsWith) { Comparison = Compare }.BuildExpression(ContextFor(context, nameof(EndsWith), EndsWith));*/
-	//if (NotStartsWith != null)
-	//  expression = expression.Combine(Expression.Not(new StringFilterOptionsAttribute(StringFilterOption.StartsWith) { Comparison = Compare }.BuildExpression(ContextFor(context, nameof(NotStartsWith), NotStartsWith))), CombineWith);
-	//if (NotEndsWith != null)
-	//  expression = expression.Combine(Expression.Not(new StringFilterOptionsAttribute(StringFilterOption.EndsWith) { Comparison = Compare }.BuildExpression(ContextFor(context, nameof(NotEndsWith), NotEndsWith))), CombineWith);
 }
