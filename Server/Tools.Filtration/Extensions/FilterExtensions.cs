0c35cfa168b8d71dd6b841de3d92417cf3dd104a
Ruslan <ilyin-r@mail.ru>
2024-03-29 02:09:33 +0300

    Process group

--- Server/Tools.Filtration/Extensions/FilterExtensions.cs
+++ Server/Tools.Filtration/Extensions/FilterExtensions.cs
@@ -14,50 +14,9 @@ public static class FilterExtensions {
 		this IQueryable<TEntity> query,
 		FilterGroup<TFilterModel> filter
 	) where TFilterModel : IFilter {
-		var lambda = BuildLambdaExpression<TEntity, TFilterModel>(filter);
+		var lambda = filter.BuildLambdaExpression<TEntity, TFilterModel>();
 		return lambda is null ? query : query.Where(lambda);
 	}
-
-	private static Expression<Func<TEntity, bool>>? BuildLambdaExpression<TEntity, TFilterModel>(
-		FilterGroup<TFilterModel> filter
-	) where TFilterModel : IFilter {
-		var parameter = Expression.Parameter(typeof(TEntity), "x");
-		var body = BuildBodyExpression(typeof(TEntity), filter, parameter);
-
-		return body is null
-			? null
-			: Expression.Lambda<Func<TEntity, bool>>(body, parameter);
-	}
-
-	private static Expression? BuildBodyExpression<TFilterModel>(
-		Type targetType,
-		FilterGroup<TFilterModel> filter,
-		Expression parameter
-	) where TFilterModel : IFilter {
-		Expression? bodyExpression = null;
-
-		foreach (var (filterProperty, targetProperty) in GetPropertyPairs(targetType, filter)) {
-			if (filterProperty.GetValue(filter) is not IFilterOperator filterOperator) continue;
-
-			var target = Expression.Property(parameter, targetProperty.Name);
-
-			var expression = filterOperator.BuildExpressionFor(target);
-			if (expression is not null)
-				bodyExpression = expression.Combine(bodyExpression, CombineType.And);
-		}
-
-		return bodyExpression;
-	}
-
-	private static IEnumerable<PropertyPair> GetPropertyPairs<TFilterModel>(
-		Type targetType, FilterGroup<TFilterModel> filter
-		) where TFilterModel : IFilter =>
-		filter
-			.GetType()
-			.GetProperties()
-			.Select(filterProperty =>
-				(filterProperty, targetProperty: targetType.GetProperty(filterProperty.Name)))
-			.Where(tuple => tuple.targetProperty is not null)!;
 
 	public static IQueryable<TEntity> ApplyFilter<TEntity>(this IQueryable<TEntity> query, IFilter filter) {
 		var lambda = BuildLambdaExpression<TEntity>(filter);
@@ -68,9 +27,7 @@ public static class FilterExtensions {
 		var parameter = Expression.Parameter(typeof(TEntity), "x");
 		var body = BuildBodyExpression(typeof(TEntity), filter, parameter);
 
-		return body is null
-			? null
-			: Expression.Lambda<Func<TEntity, bool>>(body, parameter);
+		return body is null ? null : Expression.Lambda<Func<TEntity, bool>>(body, parameter);
 	}
 
 	private static Expression? BuildBodyExpression(Type targetType, IFilter filter, Expression parameter) {
