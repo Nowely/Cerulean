89fb396e87d1fbe45f9326a769d70ceddc468812
Ruslan <ilyin-r@mail.ru>
2024-03-29 01:05:31 +0300

    Clear

--- Server/Tools.Filtration/Abstractions/IFilterOperator.cs
+++ Server/Tools.Filtration/Abstractions/IFilterOperator.cs
@@ -4,11 +4,6 @@ namespace Tools.Filtration.Abstractions;
 
 /// <summary>
 /// Any property type which is able to <see cref="BuildExpressionFor"/> over source property.
-/// <list type="table">
-/// <item>
-/// You can create new Complex Types via implementing this interface. It'll be automatically called if defined in an object which is implements <see cref="IFilter"/>.
-/// </item>
-/// </list>
 /// </summary>
 public interface IFilterOperator
 {
